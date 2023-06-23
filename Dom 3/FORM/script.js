5c4a47d2685f2d6f3d266457d26d52abcf24554f
SATHYASEELAN <128250224+sathyaseelan3@users.noreply.github.com>
2023-06-24 00:33:10 +0530

    script.js

--- Dom 3/FORM/script.js
+++ Dom 3/FORM/script.js
@@ -1,49 +1,48 @@
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    var firstName = document.getElementById('firstName').value;
-    var lastName = document.getElementById('lastName').value;
-    var address = document.getElementById('address').value;
-    var pincode = document.getElementById('pincode').value;
-    var gender = document.getElementById('gender').value;
-    var foodOptions = document.getElementById('food').selectedOptions;
-    var foods = Array.from(foodOptions).map(function(option) {
-      return option.value;
-    });
-    var state = document.getElementById('state').value;
-    var country = document.getElementById('country').value;
-  
-    if (foods.length < 2) {
-      showMessage('error', 'Please choose at least 2 food options');
-      return;
-    }
-  
-    var newRow = document.createElement('tr');
-    newRow.innerHTML = '<td>' + firstName + '</td>' +
-                       '<td>' + lastName + '</td>' +
-                       '<td>' + address + '</td>' +
-                       '<td>' + pincode + '</td>' +
-                       '<td>' + gender + '</td>' +
-                       '<td>' + foods.join(', ') + '</td>' +
-                       '<td>' + state + '</td>' +
-                       '<td>' + country + '</td>';
-  
-    document.querySelector('#dataTable tbody').appendChild(newRow);
-    showMessage('success', 'Record added successfully');
-  
-    document.getElementById('myForm').reset();
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var firstName = document.getElementById('first-name').value;
+  var lastName = document.getElementById('last-name').value;
+  var address = document.getElementById('address').value;
+  var pincode = document.getElementById('pincode').value;
+  var gender = document.querySelector('input[name="gender"]:checked').value;
+  var foodOptions = document.getElementById('food').selectedOptions;
+  var foods = Array.from(foodOptions).map(function(option) {
+    return option.value;
   });
-  
-  function showMessage(className, message) {
-    var messageContainer = document.createElement('div');
-    messageContainer.className = className;
-    messageContainer.textContent = message;
-  
-    var form = document.getElementById('myForm');
-    form.appendChild(messageContainer);
-  
-    setTimeout(function() {
-      form.removeChild(messageContainer);
-    }, 3000);
+  var state = document.getElementById('state').value;
+  var country = document.getElementById('country').value;
+
+  if (foods.length < 2) {
+    showMessage('error', 'Please choose at least 2 food options');
+    return;
   }
-  
+
+  var newRow = document.createElement('tr');
+  newRow.innerHTML = '<td>' + firstName + '</td>' +
+                     '<td>' + lastName + '</td>' +
+                     '<td>' + address + '</td>' +
+                     '<td>' + pincode + '</td>' +
+                     '<td>' + gender + '</td>' +
+                     '<td>' + foods.join(', ') + '</td>' +
+                     '<td>' + state + '</td>' +
+                     '<td>' + country + '</td>';
+
+  document.querySelector('#dataTable tbody').appendChild(newRow);
+  showMessage('success', 'Record added successfully');
+
+  document.getElementById('form').reset();
+});
+
+function showMessage(className, message) {
+  var messageContainer = document.createElement('div');
+  messageContainer.className = className;
+  messageContainer.textContent = message;
+
+  var form = document.getElementById('form');
+  form.appendChild(messageContainer);
+
+  setTimeout(function() {
+    form.removeChild(messageContainer);
+  }, 3000);
+}
