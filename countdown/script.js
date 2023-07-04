43d6d0bfa35fb7026ebeff538e3e3944f1e38f2d
SATHYASEELAN <128250224+sathyaseelan3@users.noreply.github.com>
2023-07-04 12:09:11 +0530

    script.js

--- countdown/script.js
+++ countdown/script.js
@@ -1,20 +1,15 @@
-// Countdown function using callback
+
 function startCountdown(count, callback) {
     if (count > 0) {
-        // Display the countdown
         document.getElementById("countdown").innerHTML = count;
-        // Wait for 1 second before decreasing the count
         setTimeout(function() {
             startCountdown(count - 1, callback);
         }, 1000);
     } else {
-        // Countdown finished, call the callback function
         callback();
     }
 }
 
-// Display message function
 function displayMessage(message) {
-    // Display the message
     document.getElementById("countdown").innerHTML = message;
 }
